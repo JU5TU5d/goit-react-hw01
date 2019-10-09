9fc396e0e751bd2006b48ac0feba00bcbf10d8c1
JU5TU5d <vladislav261100@gmail.com>
2019-10-09 10:35:58 +0300

    Updates

---
+++ precache-manifest.9a98907c44a68a02fc0b8c0abed2bd75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117c27732b419d68d1d0c1c7a10d6265",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6402044dba3a71e9f126",
+    "url": "/goit-react-hw-01-components/static/css/main.04a29b3d.chunk.css"
+  },
+  {
+    "revision": "804f447c63522c129d8f",
+    "url": "/goit-react-hw-01-components/static/js/2.8b23a86e.chunk.js"
+  },
+  {
+    "revision": "6402044dba3a71e9f126",
+    "url": "/goit-react-hw-01-components/static/js/main.f181eb28.chunk.js"
+  },
+  {
+    "revision": "6e529e5db644c6f2c309",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.4f1ae2cf.js"
+  }
+]);
